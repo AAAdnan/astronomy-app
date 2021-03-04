12e5c78f499ac940dd66926d742530c023a98286
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-03-04 00:58:52 +0000

    added apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import "tailwindcss/tailwind.css";
 
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://lingering-leaf.us-west-2.aws.cloud.dgraph.io/graphql',
+  cache: new InMemoryCache()
+});
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
