5cc42a636131013f40e8684e03d3b67fe4228ba0
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-03-31 21:14:47 +0100

    added cancel

--- components/Image-Uploader.js
+++ components/Image-Uploader.js
@@ -2,9 +2,63 @@ import { useRef, useState, useEffect } from "react";
 
 const ImageUploader = ({ defaultImage }) => {
   const fileSelect = useRef(null);
-  const dropbox = useRef();
+  const dropbox = useRef(null);
   const [image, setImage] = useState(defaultImage);
   const [progress, setProgress] = useState(0);
+
+  async function handleImageUpload() {
+      if (fileSelect) {
+          fileSelect.current.click();
+      }
+  }
+
+  function handleFiles(files) {
+    console.log("handleFiles");
+    console.log(files);
+  for (let i = 0; i < files.length; i++) {
+    console.log(files[i]);
+    uploadFile(files[i]);
+  }
+}
+
+function uploadFile(file) {
+    const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME}/upload`;
+    const xhr = new XMLHttpRequest();
+
+    const fd = new FormData();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+    xhr.upload.addEventListener("progress", (e) => {
+        setProgress(Math.round((e.loaded * 100.0) / e.total));
+    });
+
+    xhr.onreadystatechange = (e) => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const response = JSON.parse(xhr.responseText);
+
+            setImage(response.secure_url);
+            console.log(response.secure_url);
+        }
+    }; 
+
+    fd.append(
+      "upload_preset",
+      process.env.NEXT_PUBLIC_CLOUDINARY_PRESET
+    );
+    fd.append("tags", "browser_upload");
+    fd.append("file", file);
+    xhr.send(fd);
+}
+
+function handleCancel() {
+    setImage(null);
+  }
+
+  function handleSave() {
+    console.log(image);
+    alert(image);
+  }
 
   useEffect(() => {
     function dragEnter(e) {
@@ -39,65 +93,43 @@ const ImageUploader = ({ defaultImage }) => {
     };
   }, []);
 
-  async function handleImageUpload() {
-    if (fileSelect) {
-      fileSelect.current.click();
-    }
-  }
-
-  function handleFiles(files) {
-    for (let i = 0; i < files.length; i++) {
-      console.log(files[i]);
-      uploadFile(files[i]);
-    }
-  }
-
-  function uploadFile(file) {
-      const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME}/upload`;
-      const xhr = new XMLHttpRequest();
-
-      const fd = new FormData();
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-
-      xhr.upload.addEventListener("progress", (e) => {
-          setProgress(Math.round((e.loaded * 100.0) / e.total));
-      });
-
-      xhr.onreadystatechange = (e) => {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-              const response = JSON.parse(xhr.responseText);
-
-              setImage(response.secure_url);
-              console.log(response.secure_url);
-          }
-      }; 
-
-      fd.append(
-        "upload_preset",
-        process.env.NEXT_PUBLIC_CLOUDINARY_PRESET
-      );
-      fd.append("tags", "browser_upload");
-      fd.append("file", file);
-      xhr.send(fd);
-  }
 
   return (
-    <>
+    <div ref={dropbox}>
     {image ? (
-      <img
-        className="object-contain rounded-lg"
-        src={image.replace("upload/", "upload/w_600/")}
-        style={{ height: 400, width: 600 }}
-      />
+      <>
+        <img
+          className="rounded-lg"
+          src={image.replace("upload/", "upload/w_600/")}
+          style={{ height: 400, width: 600 }}
+        />
+        <div className="flex justify-between items-center mt-2">
+          <button
+            className="text-gray-700 hover:text-gray-500 border-2 border-gray-300 px-4 py-2 rounded w-1/2"
+            onClick={handleCancel}
+            type="button"
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-blue-600 hover:bg-blue-800 border-2 border-blue-600 text-white px-4 py-2 rounded ml-2 w-1/2"
+            onClick={handleSave}
+            type="button"
+          >
+            Save to Album
+          </button>
+        </div>
+      </>
     ) : (
-      <div ref={dropbox}
+      <div
         className="bg-gray-200 border-4 border-dashed border-gray-400 rounded-lg"
         style={{ height: 400, width: 600 }}
       >
         <form className="flex justify-center items-center h-full">
           {progress === 0 ? (
             <div className="text-gray-700 text-center">
+              <div>Drag and Drop assets here</div>
+              <div className="my-2">or</div>
               <button
                 className="bg-blue-600 hover:bg-blue-800 text-white font-bold px-4 py-2 rounded block m-auto"
                 onClick={handleImageUpload}
@@ -120,7 +152,8 @@ const ImageUploader = ({ defaultImage }) => {
         </form>
       </div>
     )}
-  </>
+  </div>
+  
   );
 }
 
--- pages/upload.js
+++ pages/upload.js
@@ -20,8 +20,7 @@ const Upload = () => {
           <h1 className="text-3xl lg:text-5xl font-bold uppercase text-white">
           Recognise Constellation
           </h1>
-          <button type="button" class="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-white hover:bg-blue-700 text-white hover:text-white font-normal py-2 px-4 rounded">Save to Album</button>
-          <button type="button" class="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-white hover:bg-blue-700 text-white hover:text-white font-normal py-2 px-4 rounded">Identify</button>
+          <button type="button" className="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-white hover:bg-blue-700 text-white hover:text-white font-normal py-2 px-4 rounded">Identify</button>
         </div>
         <div className="pt-16">
           <ImageUploader />
