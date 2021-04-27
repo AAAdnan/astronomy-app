c16a4c3ea4eebdbaa111df1d69644101737886e3
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-04-28 00:23:53 +0100

    added photo search page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
       loader: 'cloudinary',
-      domains: ['apod.nasa.gov', 'res.cloudinary.com'],
+      domains: ['apod.nasa.gov', 'images.pexels.com'],
     },
   }
