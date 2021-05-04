3e9a7d0deee940416522f31a587e4411c58c40b4
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-05-04 17:13:12 +0100

    added nasa photo search

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import React from "react";
 import { getCuratedPhotos, getQueryPhotos } from '../lib/api'
+import { getNasaPhotos } from '../lib/nasa'
 import Link from 'next/link'
 import SearchCard from '../components/SearchCard'
 import ImageSearch from '../components/ImageSearch'
@@ -10,10 +11,27 @@ import { useQuery, useMutation, gql } from "@apollo/client"
 import { useRef, useState, useEffect } from "react";
 import image from 'next/image';
 
-const Search = ({data}) => {
-    const [photos, setPhotos] = useState(data);
+const Search = ({ items }) => {
+    const [photos, setPhotos] = useState(items);
     const [isLoading, setIsLoading] = useState(true);
-    const [term, setTerm] = useState('');
+    const [query, setQuery] = useState('');
+
+    console.log('these are the photos : ' + photos)
+
+
+    console.log(query)
+
+
+    const handleSubmit = async (e) => {
+        await e.preventDefault();
+        if (query == "") {
+         console.log('nothing searched for')
+        } else {
+          const res = await getNasaPhotos(query);
+          await setPhotos(res);
+          await setQuery("");
+        }
+      };
 
     return (
         <>
@@ -22,18 +40,29 @@ const Search = ({data}) => {
             <title>Search</title>
         </Head>
         <div className="min-h-screen bg-black overflow-hidden">
-        <ImageSearch />
             <div className="container mx-auto">
                 <div className="text-center text-6xl font-bold uppercase text-white pb-8 pt-8">
                     Photo Search
                 </div>
+                <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+                    <form onSubmit={handleSubmit} className="w-full max-w-sm">
+                        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+                        <input value={query} onChange={(e) => setQuery(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
+                        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                            Search
+                        </button>
+                        </div>
+                    </form>
+                </div>
                 <div className="grid grid-cols-3 gap-4">
-                {photos ? photos.map(photo => (
-                    <SearchCard key={photo.id} alt={photo.url} image={photo.src.medium} />
-                )
-                )
-                : <p>No images found</p>
-                }
+                {photos &&
+                    photos.map((preview) => (
+                <SearchCard
+                  key={preview.data[0].nasa_id}
+                  thumbnailUrl={preview.links[0].href}
+                  nasaId={preview.data[0].nasa_id}
+                 />
+                ))}
                 </div>
             </div>
         </div>
@@ -41,14 +70,15 @@ const Search = ({data}) => {
     );
 }
 
-export async function getServerSideProps() {
-    const data = await getCuratedPhotos();
+export async function getStaticProps() {
+    const results = await fetch(
+      "https://images-api.nasa.gov/search?media_type=image"
+    );
+    const previews = await results.json();
+    const items = await previews.collection.items;
     return {
-      props: {
-        data,
-      },
+      props: { items },
     };
   }
-
 
 export default Search;
